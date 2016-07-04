670dc86f1178a93c4736a7f6ccf126a83352ca19
galuel <galuel@glibre.org>
2016-07-04 10:14:24 +0200

    Release 0.90 avec live-accessibilite.html Signed-off-by: galuel <galuel@glibre.org>

---
+++ js/getreleases.js
@@ -0,0 +1,16 @@
+getRelease("galuel","applets-pgn4web","applets_releases");
+function getRelease(user, repo, Id) {
+   var xmlhttp = new XMLHttpRequest();
+   xmlhttp.onreadystatechange = function() {
+       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          var myArr = JSON.parse(xmlhttp.responseText);
+          release = populateRelease(myArr,Id);
+       }
+   }
+   xmlhttp.open("GET", "https://api.github.com/repos/" + user + "/" + repo + "/releases", true);
+   xmlhttp.send();
+}
+function populateRelease(arr,Id) {
+   var release = arr[0].tag_name;
+   document.getElementById(Id).innerHTML = release;
+}
